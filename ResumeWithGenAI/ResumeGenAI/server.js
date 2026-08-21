6dbda6cfab8f07d3b4b69fbcd7407aaa482c226f
Ajay Pratap Chauhan <ajaypratapchauhan786@gmail.com>
2026-08-21 18:11:53 +0530

    new change

--- ResumeWithGenAI/ResumeGenAI/server.js
+++ ResumeWithGenAI/ResumeGenAI/server.js
@@ -3,8 +3,11 @@ const app = require("./src/app")
 const connectTODB = require("./src/config/database")
 
 
+const PORT = process.env.PORT || 3000
+
+
 connectTODB()
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}` );
     
 })
